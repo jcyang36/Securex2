6db75ed1fa42a8c8868c1b5158faaa882310cb17
Joyce Yang <jcyang@hmc.edu>
2017-06-28 16:51:22 -0400

    Completed step 5, need to fix admin

--- src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
+++ src/main/java/byAJ/Securex/configs/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package byAJ.Securex.configs;
 
+import byAJ.Securex.repositories.UserRepository;
+import byAJ.Securex.services.SSUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,10 +17,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+@Autowired
+private UserRepository userRepository;
 
+@Override
+public UserDetailsService userDetailsServiceBean() throws Exception{
+    return new SSUserDetailsService(userRepository);
+}
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+
+    http
                 .authorizeRequests().antMatchers("/", "/books/list","/css/**").permitAll()
                 .antMatchers("/books/edit/**").hasRole("ADMIN")
                        .anyRequest().authenticated()
@@ -37,9 +47,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().
+
+    auth.userDetailsService(userDetailsServiceBean());
+    /*auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().
                 withUser("root").password("password2").roles("ADMIN").and().
                 withUser("Dave").password("begreat").roles("ADMIN").and().
-        withUser("Fi").password("becold").roles("ADMIN");
+        withUser("Fi").password("becold").roles("ADMIN");*/
     }
 }
